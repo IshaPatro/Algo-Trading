5022dc4818d0c34de944ac58b2ebafa1a2d39564
Isha Patro <isha.patro.215@gmail.com>
2025-03-08 17:58:59 +0530

    Deployment Fixes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import time
 from threading import Event, Thread
 from dataStream import stream_data
-import config
-from dashboard import create_app
+from dashboard import create_app as create_dashboard_app
+import dash
 
-if __name__ == "__main__":    
+app = create_dashboard_app()
+
+if __name__ == "__main__":  
     stop_event = Event()
     data_thread = Thread(target=stream_data, args=(stop_event,))
     data_thread.daemon = True
     data_thread.start()
-    
-    app = create_app()
     app.run_server(debug=False, port=8050)
+    
